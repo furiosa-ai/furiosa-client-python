2c9fab1ffd16beb70b4ad9419eb1d08025936505
Hyunsik Choi <hyunsik@furiosa.ai>
2021-03-11 22:17:42 -0800

    Add more methods to task to check the phase of tasks

--- furiosa/client/compiler.py
+++ furiosa/client/compiler.py
@@ -5,6 +5,10 @@ from furiosa.config import load_furiosa_config
 from furiosa.openapi import Configuration, ApiClient
 from furiosa.openapi.api.compiler_v1_api import CompilerV1Api
 
+PENDING = 'Pending'
+RUNNING = 'Running'
+SUCCEEDED = 'Succeeded'
+FAILED = 'Succeeded'
 
 class CompileTask(object):
     def __init__(self, api: CompilerV1Api, compile_task):
@@ -24,17 +28,26 @@ class CompileTask(object):
         if phase == 'Failed':
             raise BaseException(self.api.get_log(self.compile_task.task_id))
 
+    def is_succeeded(self):
+        return self.compile_task.phase == SUCCEEDED
+
+    def is_failed(self):
+        return self.compile_task.phase == FAILED
+
+    def is_completed(self):
+        return self.is_succeeded() or self.is_failed()
+
     def phase(self) -> str:
         return self.compile_task.phase
 
     def list_artifacts(self):
-        if self.compile_task.phase == 'Succeeded':
+        if self.compile_task.phase == SUCCEEDED:
             return self.api.list_artifacts(task_id=self.compile_task.task_id)
         else:
             raise BaseException(self.api.get_log(self.compile_task.task_id))
 
     def executable(self):
-        if self.compile_task.phase == 'Succeeded':
+        if self.compile_task.phase == SUCCEEDED:
             response = self.api.get_artifact(task_id=self.task_id(),
                                                   name='output.enf',
                                                   _preload_content=False)
