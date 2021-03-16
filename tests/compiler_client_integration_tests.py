24f2cf63078b48010b58e34eda8c521dbaed34c7
Hyunsik Choi <hyunsik@furiosa.ai>
2021-03-15 17:17:37 -0700

    Improve the integration tests

--- tests/compiler_client_integration_tests.py
+++ tests/compiler_client_integration_tests.py
@@ -17,15 +17,21 @@ class TestCompilerClient(unittest.TestCase):
         with open(test_data('MNISTnet_uint8_quant_without_softmax.tflite'), 'rb') as file:
             compile_task = self.client.submit_compile(source=file)
             compile_task.wait_for_complete()
-            print(compile_task.list_artifacts())
-            len(compile_task.get_ir())
+            self.assertEqual(compile_task.phase(), 'Succeeded')
+            self.assertIsNotNone(compile_task.list_artifacts())
+            self.assertIsNotNone(compile_task.get_ir())
+            self.assertIsNotNone(compile_task.get_logs())
+            self.assertIsNotNone(compile_task.get_compiler_report())
+            self.assertIsNotNone(compile_task.get_dot_graph())
+            self.assertIsNotNone(compile_task.get_memory_alloc_report())
 
     def test_compile_with_configs(self):
         with open(test_data('MNISTnet_uint8_quant_without_softmax.tflite'), 'rb') as file:
-            compile_task = self.client.submit_compile(source=file, compiler_config={'keep_unsignedness': True}, target_npu_spec={})
+            compile_task = self.client.submit_compile(source=file,
+                                                      compiler_config={'keep_unsignedness': True},
+                                                      target_npu_spec={})
             compile_task.wait_for_complete()
-            print(compile_task.list_artifacts())
-            len(compile_task.get_ir())
+            self.assertEqual(compile_task.phase(), 'Succeeded')
 
 
 if __name__ == '__main__':
